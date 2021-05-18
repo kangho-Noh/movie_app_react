81a9b5535ebb17e3e23e88c229966e7b55adbba5
노강호 <rkdgh98@khu.ac.kr>
2021-05-18 23:44:49 +0900

    axios로 API 가져오기, API 데이터 가져오는 함수 동기화 하는 방법 aync, await

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import axios from "axios";
 
 class App extends React.Component{  
   state={
@@ -8,16 +8,30 @@ class App extends React.Component{
   }; //state에 사용할 변수를 미리 선언하지 않아도 된다. setState만 써도 됨.
   //하지만 이렇게 써놓는 것이 코드 읽기에 편하기 때문에 선언하는 습관을 들이는 것이 좋겠다.
   
+//함수 비동기화. axios가 데이터를 불러오기를 기다려준다. async와 await은 한 세트
+getMovies = async ()=>{
+  const movies = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json");
+  return movies;
+}
 
   //componentDidMount에서 이론적으로 data를 fetch한다
   componentDidMount(){
-    
-    console.log("DidMount start");
-    //6초 후에 익명함수 실행. setState 문법 
-    setTimeout(() => {
-      this.setState({isLoading : false});
-    }, 6000);
-    console.log("DidMount done"); //얘는 비동기식이라 setTimeout하는 동안 실행끝남.
+     /*
+    axios = fetch 위에 있는 작은 layer
+    npm install axios
+
+    YTS의 API를 사용할 것이다. yts.mx/api접속
+    YTS는 불법적으로 영화 올리는 곳임. 그래서 매번 URL이 바뀌어서 API도 바뀐다.
+    그래서 니콜라스가 YTS Proxy라는 것을 만들어서 사용하면 매번 바뀌는 URL에 대응할 수 있다
+    https://yts-proxy.nomadcoders1.now.sh/list_movies.json
+    */
+   /* 동기화
+   axios는 느리기 때문에 동기화가 필요하다.
+   방법 1. componentDidMount() 앞에 async 를 붙여 함수 자체를 비동기식 함수로 바꾼다
+   방법 2. getMovies라는 async함수를 만들어서 그 안에서 axios를 await으로 실행한다.
+   여기서는 방법 2로 구현해볼 것임.
+   */
+    this.getMovies();
   }
 
   render(){
