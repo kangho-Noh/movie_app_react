ece7b529b82959c3849ef63ee366c6405f3ab57a
노강호 <rkdgh98@khu.ac.kr>
2021-05-21 01:21:44 +0900

    #4.4 css added

--- src/Movie.js
+++ src/Movie.js
@@ -6,12 +6,12 @@ import "./Movie.css";
 //css를 하기 위해서 style={{}}를 사용할 수 있다.
 function Movie({ id, year, title, summary, poster, genres }) {
   return (
-    <div className="movies__movie">
+    <div className="movie">
       <img src={poster} alt={title} title={title} />
       <div className="movie__data">
         <h3 className="movie__title">{title}</h3>
         <h5 className="movie__year">{year}</h5>
-        <ul className="genres">
+        <ul className="movie__genres">
           {genres.map((genre, index) => (
             //map은 (현재값, 인덱스) 두개의 아규먼트를 넘김
             //key값이 없으면 오류가 나기때문에 index로 key를 만들어서 제공한다
